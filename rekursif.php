9c021d623648f0455f6a23d3b278c1f4edf37cae
fitrianar <42660766+fitrianar@users.noreply.github.com>
2018-09-13 08:59:40 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,41 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$i=5;
+function rekursif($j, $k){
+	if ($j>0){
+		if ($k>0) {
+			echo("* ");
+			$k--;
+			rekursif($j, $k);
+		}
+		else {
+			echo("<br>");
+			$j--;
+			rekursif($j, $j);
+		}
+	}
 }
-rekursif(5);
+
+rekursif($i, $i);
+
+echo "----------";
+
+$n=5;
+function rekursif2($m, $l, $n){
+	if ($m<=$n){
+		if ($l<=$m) {
+			echo ($l);
+			$l++;
+			rekursif2($m, $l, $n);
+		}
+		else {
+			echo "<br>";
+			$m++;
+			rekursif2($m, 1, $n);
+		}
+	}
+}
+
+rekursif2(1, 1, $n);
+
+
 ?>
